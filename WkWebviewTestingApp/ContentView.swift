5c0f667c3e648ebff2e4dee4e19e619e76446ed5
Andrzej Chmielewski <andrzejchm@gmail.com>
2023-10-26 14:03:08 +0200

    add wkwebview implementation

--- WkWebviewTestingApp/ContentView.swift
+++ WkWebviewTestingApp/ContentView.swift
@@ -1,22 +1,45 @@
-//
-//  ContentView.swift
-//  WkWebviewTestingApp
-//
-//  Created by Andrzej Chmielewski on 26/10/2023.
-//
-
 import SwiftUI
+import WebKit
+
+// 1. Create a UIViewRepresentable for WKWebView
+struct WebView: UIViewRepresentable {
+    @Binding var urlString: String
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let webConfiguration = WKWebViewConfiguration()
+        webConfiguration.allowsInlineMediaPlayback = true
+               webConfiguration.preferences.setValue(true, forKey: "developerExtrasEnabled")
+               
+               let webView = WKWebView(frame: .zero, configuration: webConfiguration)
+               return webView
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let url = URL(string: urlString) {
+            let request = URLRequest(url: url)
+            uiView.load(request)
+        }
+    }
+}
 
 struct ContentView: View {
+    @State private var urlString: String = "https://secure-me.au10tixservices.com/app.html?api=aHR0cHM6Ly9ldXMtYXBpLmF1MTB0aXhzZXJ2aWNlcy5jb20vc2VjdXJlLW1lL3Yy&bcd=%231f1d2a&bcl=%23ffffff&isWebView=true&pc=%23040024&themeName=light&token=OpX9FeaC0y4oHDfpYkaZ" // Default URL
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("Enter URL", text: $urlString, onCommit: {
+                // Hide keyboard
+                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+            })
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .padding()  // This padding respects safe areas by default
+            
+            WebView(urlString: $urlString)
+                .edgesIgnoringSafeArea(.all)  // Only the WebView will ignore safe areas
         }
-        .padding()
     }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
