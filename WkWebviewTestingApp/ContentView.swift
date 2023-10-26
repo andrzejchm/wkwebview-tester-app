3b343d5a4d40167745540b1f941f3b144f6ffcaa
Andrzej Chmielewski <andrzejchm@gmail.com>
2023-10-26 09:56:24 +0200

    Initial Commit

---
+++ WkWebviewTestingApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WkWebviewTestingApp
+//
+//  Created by Andrzej Chmielewski on 26/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WkWebviewTestingApp/WkWebviewTestingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WkWebviewTestingAppApp.swift
+//  WkWebviewTestingApp
+//
+//  Created by Andrzej Chmielewski on 26/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct WkWebviewTestingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
